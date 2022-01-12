b7f462c76d43dac71f137c84ac6eae6230be7534
soberanoJr <tonysoberano@gmail.com>
2022-01-12 10:33:35 -0300

    'makemigrations' and 'migrate'

--- modules/models.py
+++ modules/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Module(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"Module #{self.id}: {self.name}"
+
+
+class Lesson(models.Model):
+    name = models.CharField(max_length=64)
+    date = models.DateField()
+    module = models.ManyToManyField(Module, blank=True, related_name="lessons")
+
+    def __str__(self):
+        return f"{self.name} | {self.date}"
--- modules/views.py
+++ modules/views.py
@@ -1,3 +1,24 @@
+from django import forms
+from django.http import HttpResponseBadRequest, HttpResponseRedirect, Http404
 from django.shortcuts import render
+from django.urls import reverse
+
+from .models import Module, Lesson
 
 # Create your views here.
+def index(request):
+    return render(request, "modules/index.html", {
+        "modules": Module.objects.all()
+    })
+
+
+def module(request, module_id):
+    try:
+        module = Module.objects.get(id=module_id)
+    except Module.DoesNotExist:
+        raise Http404("Module not found.")
+    return render(request, "modules/module.html", {
+        "module": module,
+        "lessons": module.lessons.all(),
+        "non_lessons": Lesson.objects.exclude(modules=module).all()
+    })
